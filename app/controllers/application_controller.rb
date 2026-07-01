8e9b6ad7d17416def67b7920b5c8979750117470
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-07-01 12:58:49 +0900

    add: deviseの追加と初期設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+  # 新規登録するときはニックネームを許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end  
 end
