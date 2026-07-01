62ca4fce496acae0295b08703f87995861da8548
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-07-01 09:35:39 +0900

    add: Home#indexの実装・テスト追加

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/home/index'
-      expect(response).to have_http_status(:success)
+    it 'HTTPステータス 200 を返す' do
+      get '/'
+      expect(response).to have_http_status(200)
     end
   end
 end
