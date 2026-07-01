5a6c82b0a7e3c1fe788908219e22dd59641804ed
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-07-01 22:15:22 +0900

    add: Postモデル作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
  
-# メデルのテストを明示しなくとも良い User, type: :model
+# modelのテストを明示しなくとも良い User, type: :model
 describe User do
   # シンボル形式　テストでの型
   let(:nickname) { '山田花子' }
@@ -10,16 +10,24 @@ describe User do
   describe '.first' do
     # テストが実行される前の処理
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email)
+      @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id)
     end
 
     # described_classは Userクラスを指す
     # subject変数に入れる
     subject { described_class.first }
-
+    
     it '事前に作成した通りのUserを返す' do
       expect(subject.nickname).to eq('山田花子')
       expect(subject.email).to eq('test@example.com')
+    end
+
+    it '紐づくPostの情報を取得できる' do
+      expect(subject.posts.size).to eq(1)
+      expect(subject.posts.first.title).to eq('タイトル')
+      expect(subject.posts.first.content).to eq('本文')
+      expect(subject.posts.first.user_id).to eq(@user.id)
     end
   end
 
