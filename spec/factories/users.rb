a6c5782ec1278a17e8fe5779dca49c0a0c362ea0
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-07-01 13:52:53 +0900

    add: User.nicknameの文字列に関するバリデーションを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { '山田花子' }
+    # 連番で生成
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
+ 
+# メデルのテストを明示しなくとも良い User, type: :model
+describe User do
+  # シンボル形式　テストでの型
+  let(:nickname) { '山田花子' }
+  let(:email) { 'test@example.com' }
+  let(:password) { '12345678' }
+  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) } 
+  describe '.first' do
+    # テストが実行される前の処理
+    before do
+      create(:user, nickname: nickname, email: email)
+    end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # described_classは Userクラスを指す
+    # subject変数に入れる
+    subject { described_class.first }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.nickname).to eq('山田花子')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
+
+  describe 'validation' do
+    describe 'nickname属性' do
+      describe '文字数制限の検証' do
+        context 'nicknameが20文字以下の場合' do
+          # コンテキストの中でオブジェクトを上書き
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } #20文字
+          it 'Userオブジェクトは有効' do
+            expect(user.valid?).to be(true)
+          end
+        end
+        context 'nicknameが20文字を超える場合' do
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
+ 
+          it 'User オブジェクトは無効である' do
+            expect(user.valid?).to be(false)
+            # userオブジェクトのerrorsにはエラー情報が格納されている
+            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+          end
+        end
+      end
+    end
+  end
 end
